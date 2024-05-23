bc91aea3b198cd0fdf74fa324e4bd19b0862894d
Nicholas Safronov <102466423+wolfychanOwO@users.noreply.github.com>
2024-05-23 17:19:10 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,9 +1,23 @@
 from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+import logging
 
-from botinstance import bot_instance
-from db import create_async_engine, get_session_maker
-from settings import DB_CONNECTION_STRING
 
-# storage = MemoryStorage()
+storage = MemoryStorage()
 dp = Dispatcher()
+pool = None
+
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {'DEBUG': '\033[94m', 'INFO': '\033[92m', 'WARNING': '\033[93m',
+              'ERROR': '\033[91m', 'CRITICAL': '\033[95m'}
+
+    def format(self, record):
+        log_fmt = f"{self.COLORS.get(record.levelname, '')}%(message)s\033[0m"
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                    handlers=[logging.StreamHandler()])
+logger = logging.getLogger(__name__)
